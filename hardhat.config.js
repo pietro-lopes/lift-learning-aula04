5304a0f66c9cac7233d8eaeba799782ab0d04365
unknownrj <unknownrj@gmail.com>
2022-08-13 14:54:38 -0300

    Added Mumbai, Deploy task, Public RPCs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,36 @@
+const { task } = require('hardhat/config');
+
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
 
-const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
-const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
-const INFURA_URL = process.env.INFURA_URL;
+const { WALLET_PRIVATE_KEY, POLYGONSCAN_API_KEY } = process.env
+
+task("deploy", "Deploy contract, use --contract <address> [--args <constructor-args>]'")
+  .addParam("contract","Contract name")
+  .addOptionalParam("args", "Contract construtor arguments")
+  .setAction(async (taskArgs) => {
+    const [deployer] = await ethers.getSigners();
+
+    console.log(`Deploying contracts with the account:, ${deployer.address}`);
+  
+    console.log(`Account balance:, ${(await deployer.getBalance()).toString()}`);
+  
+    const Token = await ethers.getContractFactory(taskArgs.contract);
+    const token = taskArgs.args ? await Token.deploy(taskArgs.args) : await Token.deploy()
+  
+    console.log(`Contract address:, ${token.address}`);
+  });
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
     matic: {
-      url: INFURA_URL,
+      url: 'https://rpc.ankr.com/polygon',
+      accounts: [`0x${WALLET_PRIVATE_KEY}`]
+    },
+    mumbai: {
+      url: 'https://rpc.ankr.com/polygon_mumbai',
       accounts: [`0x${WALLET_PRIVATE_KEY}`]
     },
   },
