d30df48e9404df0f6a964fa8129c4ca40f7e4564
unknownrj <unknownrj@gmail.com>
2022-08-13 14:36:43 -0300

    Removed console.log from imports to save gas

--- contracts/Tarefa01.sol
+++ contracts/Tarefa01.sol
@@ -3,10 +3,8 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-import "hardhat/console.sol";
-
 contract Tarefa01 is ERC20 {
     constructor() ERC20("Tarefa01", "T1L") {
-        _mint(msg.sender, 10000 * 10 ** decimals());
+        _mint(msg.sender, 10000 * 10**decimals());
     }
 }
